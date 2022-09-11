2c83e249c723174379d17722334d5639ab6692dd
ruda <ruda.rabello.silva@gmail.com>
2022-09-11 11:50:45 -0300

    feat add routes to credentials

--- src/routes/credentialRouter.ts
+++ src/routes/credentialRouter.ts
@@ -6,9 +6,26 @@ import * as credentialController from "../controllers/credentialController";
 
 const credentialRouter = Router();
 
-credentialRouter.post("/categories/credentials/create",credentialValidation,getUserData,credentialController.createCredential);
-credentialRouter.get("/credentials",getUserData,credentialController.getCredentials);
-credentialRouter.get("/credentials/:id",getUserData,credentialController.getCredentialById);
-credentialRouter.delete("/credentials/delete/:id",getUserData,credentialController.deleteCredentialById);
+credentialRouter.post(
+    "/categories/credentials/create",
+    credentialValidation,
+    getUserData,
+    credentialController.createCredential
+);
+credentialRouter.get(
+    "/credentials",
+    getUserData,
+    credentialController.getCredentials
+);
+credentialRouter.get(
+    "/credentials/:id",
+    getUserData,
+    credentialController.getCredentialById
+);
+credentialRouter.delete(
+    "/credentials/delete/:id",
+    getUserData,
+    credentialController.deleteCredentialById
+);
 
 export default credentialRouter;
